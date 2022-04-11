1377c75a36940f1f82098f8b87bd6af88ff2f2d6
vish <vd@yahoo.com>
2022-04-11 11:46:14 +0530

    changes done

--- js/myapp.js
+++ js/myapp.js
@@ -1,10 +1,10 @@
-let normaldataButton = document.querySelector (selectors '#normal-data');
-normaldataButton = addEventListner (type 'click', listener function () {
+let normalDataButton = document.querySelector(selectors:'#normal-btn');
+normalDataButton = addEventListner (type 'click', listener function () {
   //ajax request
  let ajt = new_XMLHttpRequest ();
 
  //request
-ajt.open(metohd:'GET',ulr'Data/Text-data.text' , async true);
+ajt.open(metohd:'GET',ulr'Data/Text-data.text' , async: true);
 
 //send new_XMLHttpRequest
 
@@ -22,5 +22,22 @@ ajt.onload = () => {
 //display text Data
 let displayNormalData = (data) => {
   let htmlTemplate = '<h3>$ (data)</h3>';
-  document.querySelector (selectors '#normal-data').innerHTML = htmlTemplate;
+  document.querySelector (selectors '#normal-btn').innerHTML = htmlTemplate;
 };
+
+
+//JSON data College
+
+let jsonButton = document.querySelector (selectors: '#json-btn');
+jsonButton = addEventListner(type 'click', listener function (){
+  let xhr = new new_XMLHttpRequest ();
+  xhr.open (metohd: 'GET', url: 'Data/user-data.json', async: true);
+  xhr.open ();
+  xhr.onload = () => {
+    if (xhr.status === 200){
+      let data = xhr.responseText;
+      console.log(data);
+    }
+  }
+
+})
